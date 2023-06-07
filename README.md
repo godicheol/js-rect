1aeb3b0327141581dee7ede130a355091b3b1c2e
godicheol <godicheol@gmail.com>
2023-06-07 20:31:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+## Usage
+
+```js
+import jsRect from "js-rect";
+```
+
+```js
+// Rect 100 x 200
+var sw = 100;
+var sh = 200;
+
+// Rect 1000 x 1000
+var dw = 1000;
+var dh = 1000;
+
+var deg = 45;
+
+cover(sw, sh, dw, dh); // {width: 1000, height: 2000}
+contain(sw, sh, dw, dh); // {width: 500, height: 1000}
+into(sw, sh, dw, dh); // {width: 500, height: 1000}
+into(dw, dh, sw, sh); // {width: 100, height: 100}
+rotate(sw, sh, deg); // {width: 212.13203435596427, height: 212.13203435596427}
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+// Universal module definition
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS
+    module.exports = factory();
+  } else {
+    // Browser
+    root.jsRect = factory();
+  }
+})(this, function() {
+  'use strict';
+
+  function cover(sw, sh, dw, dh) {
+    var ar = sw / sh;
+    if (ar < dw / dh) {
+      return {
+        width: dw,
+        height: dw / ar
+      }
+    } else {
+      return {
+        width: dh * ar,
+        height: dh
+      }
+    }
+  }
+
+  function contain(sw, sh, dw, dh) {
+    var ar = sw / sh;
+    if (ar < dw / dh) {
+      return {
+        width: dh * ar,
+        height: dh
+      }
+    } else {
+      return {
+        width: dw,
+        height: dw / ar
+      }
+    }
+  }
+
+  function into(sw, sh, mxw, mxh, mnw, mnh) {
+    var ar = sw / sh;
+    if (!mnw) {
+      mnw = 0;
+    }
+    if (!mnh) {
+      mnh = 0;
+    }
+    if (ar < mxw / mxh) {
+      mxw = mxh * ar;
+    } else {
+      mxh = mxw / ar;
+    }
+    if (ar < mnw / mnh) {
+      mnh = mnw / ar;
+    } else {
+      mnw = mnh * ar;
+    }
+    return {
+      width: Math.max(mxw, Math.min(mnw, sw)),
+      height: Math.max(mxh, Math.min(mnh, sh))
+    }
+  }
+
+  function rotate(sw, sh, deg) {
+    var radians = deg * Math.PI / 180;
+    var sinFraction = Math.sin(radians);
+    var cosFraction = Math.cos(radians);
+    if (sinFraction < 0) {
+      sinFraction = -sinFraction;
+    }
+    if (cosFraction < 0) {
+      cosFraction = -cosFraction;
+    }
+    return {
+      width: (sw * cosFraction) + (sh * sinFraction),
+      height: (sw * sinFraction) + (sh * cosFraction)
+    }
+  }
+
+  return {
+    cover: cover,
+    contain: contain,
+    into: into,
+    rotate: rotate,
+  }
+});
